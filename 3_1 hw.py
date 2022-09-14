f27e297c54a1de9fa5f2bc43d6e5d9d036c7fe2f
ikuklos <ikuklos@yandex.ru>
2022-09-15 00:06:56 +0300

    homework #3 example 1

---
+++ 3_1 hw.py
@@ -0,0 +1,14 @@
+# Задайте список из нескольких чисел. Напишите программу, которая найдёт сумму элементов списка, стоящих на нечётной позиции.
+# Пример:
+# - [2, 3, 5, 9, 3] -> на нечётных позициях элементы 3 и 9, ответ: 12
+
+from random import sample as sample
+
+testList = sample(range(30), 10)
+print(testList)
+sumElements = 0
+
+for oddNumber in range(0, len(testList), 2):
+    sumElements += testList[oddNumber]
+
+print(sumElements)
